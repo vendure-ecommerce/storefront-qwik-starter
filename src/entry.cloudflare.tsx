1f96fdcd3eaf15b3c025c3bc62bbc556b36d9d58
gioboa <giorgiob.boa@gmail.com>
2022-08-02 20:49:35 +0200

    change entry.cloudflare

--- src/entry.cloudflare.tsx
+++ src/entry.cloudflare.tsx
@@ -1,52 +1,10 @@
+import { qwikCity } from '@builder.io/qwik-city/middleware/cloudflare-pages';
 import render from './entry.ssr';
 
-/**
- * Cloudflare Pages Request Handler
- */
-export const onRequest: any = async ({ request, next, waitUntil }: any) => {
-	try {
-		const url = new URL(request.url);
+const qwikCityMiddleware = qwikCity(render, {
+	prefetchStrategy: {
+		implementation: 'link-prefetch',
+	},
+});
 
-		// Handle static assets
-		if (/\.\w+$/.test(request.url)) {
-			return next(request);
-		}
-
-		// Do not using caching during development
-		const useCache = url.hostname !== 'localhost' && request.method === 'GET';
-
-		// Early return from cache
-		const cache = await caches.open('custom:qwik');
-		if (useCache) {
-			const cachedResponse = await cache.match(request);
-			if (cachedResponse) {
-				return cachedResponse;
-			}
-		}
-
-		// Generate Qwik SSR HTML
-		const result: any = await render({
-			url: request.url,
-		} as any);
-
-		// Create HTTP Response
-		const response = new Response(result.html, {
-			headers: {
-				'Content-Type': 'text/html; charset=utf-8',
-				'Cache-Control': useCache
-					? `max-age=60, s-maxage=10, stale-while-revalidate=604800, stale-if-error=604800`
-					: `no-cache, max-age=0`,
-			},
-		});
-
-		if (useCache) {
-			waitUntil(cache.put(request, response.clone()));
-		}
-
-		// Return Qwik SSR response
-		return response;
-	} catch (e) {
-		// 500 Error
-		return new Response(String(e), { status: 500 });
-	}
-};
+export const onRequestGet = [qwikCityMiddleware];
