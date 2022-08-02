80da3914ad1531a85ea8c67f211d4ac621a7ae0a
gioboa <giorgiob.boa@gmail.com>
2022-08-02 20:53:19 +0200

    change root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,13 @@
 import { Html } from '@builder.io/qwik-city';
 import { Body } from './components/body/body';
 import { Head } from './components/head/head';
-
 import './global.css';
 
-export default () => {
+export default function Root() {
 	return (
-		<Html lang="en">
+		<Html>
 			<Head />
 			<Body />
 		</Html>
 	);
-};
+}
