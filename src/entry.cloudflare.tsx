9fdc01a3ddd0f025f33a5b331a7fa2e86728abcb
Glenn Becker <nnelgxorz@gmail.com>
2022-08-03 11:25:29 -0600

    Use worker prefetch strategy (#5)
    * Use worker prefetch strategy
    
    * Remove QwikCityPlan

--- src/entry.cloudflare.tsx
+++ src/entry.cloudflare.tsx
@@ -1,11 +1,12 @@
 import { qwikCity } from '@builder.io/qwik-city/middleware/cloudflare-pages';
-import qwikCityPlan from '@qwik-city-plan';
 import render from './entry.ssr';
 
 /**
  * Cloudflare Pages Request Handler
  */
 export const onRequest = async (args: any) => {
-	const handler = qwikCity(render, { ...qwikCityPlan, implementation: 'link-prefetch' });
+	const handler = qwikCity(render, {
+		prefetchStrategy: { implementation: 'worker-fetch' },
+	});
 	return await handler(args);
 };
