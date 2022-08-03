fca922d633368c9b9f5d09fb6d94c5d2b8b0ff5f
GlennBecker <nnelgxorz@gmail.com>
2022-08-03 16:55:49 +0000

    Use worker prefetch strategy

--- src/entry.cloudflare.tsx
+++ src/entry.cloudflare.tsx
@@ -6,6 +6,9 @@ import render from './entry.ssr';
  * Cloudflare Pages Request Handler
  */
 export const onRequest = async (args: any) => {
-	const handler = qwikCity(render, { ...qwikCityPlan, implementation: 'link-prefetch' });
+	const handler = qwikCity(render, {
+		...qwikCityPlan,
+		prefetchStrategy: { implementation: 'worker-fetch' },
+	});
 	return await handler(args);
 };
