fe39c3cc7c213f0e69cad5119fe4978d8614e41f
GlennBecker <nnelgxorz@gmail.com>
2022-08-03 20:30:28 +0000

    Update entry to match current starter configuration

--- src/entry.cloudflare.tsx
+++ src/entry.cloudflare.tsx
@@ -1,14 +1,13 @@
 import { qwikCity } from '@builder.io/qwik-city/middleware/cloudflare-pages';
-import qwikCityPlan from '@qwik-city-plan';
 import render from './entry.ssr';
 
 /**
  * Cloudflare Pages Request Handler
  */
-export const onRequest = async (args: any) => {
-	const handler = qwikCity(render, {
-		...qwikCityPlan,
-		prefetchStrategy: { implementation: 'worker-fetch' },
-	});
-	return await handler(args);
-};
+const qwikCityMiddleware = qwikCity(render, {
+	prefetchStrategy: {
+		implementation: 'link-prefetch',
+	},
+});
+
+export const onRequest = [qwikCityMiddleware];
