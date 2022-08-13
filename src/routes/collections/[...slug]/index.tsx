a582f7ad11ab32ffc98dcd2544a27e9ca0a8c671
GlennBecker <nnelgxorz@gmail.com>
2022-08-12 18:33:41 -0600

    Filter/Search resource

--- src/routes/collections/[...slug]/index.tsx
+++ src/routes/collections/[...slug]/index.tsx
@@ -1,4 +1,11 @@
-import { $, component$, Resource, useClientEffect$, useMount$, useStore } from '@builder.io/qwik';
+import {
+	$,
+	component$,
+	Resource,
+	useClientEffect$,
+	useResource$,
+	useStore,
+} from '@builder.io/qwik';
 import { RequestHandler, useEndpoint, useLocation } from '@builder.io/qwik-city';
 import Breadcrumbs from '~/components/breadcrumbs/Breadcrumbs';
 import CollectionCard from '~/components/collection-card/CollectionCard';
@@ -14,17 +21,20 @@ import { Collection, FacetWithValues, Search } from '~/types';
 import { changeUrlParamsWithoutRefresh, enableDisableFacetValues, groupFacetValues } from '~/utils';
 import { execute } from '~/utils/api';
 
+export interface SearchData {
+	search: Search;
+	facedValues: FacetWithValues[];
+}
+
 export default component$(() => {
 	const { params, query } = useLocation();
 	const activeFacetValueIds: string[] = query.f ? [query.f] : [];
 	const state = useStore<{
-		loading: boolean;
 		showMenu: boolean;
 		search: Search;
 		facedValues: FacetWithValues[];
 		facetValueIds: string[];
 	}>({
-		loading: false,
 		showMenu: false,
 		search: {} as Search,
 		facedValues: [],
@@ -32,17 +42,26 @@ export default component$(() => {
 	});
 
 	const collection = useEndpoint<typeof onGet>();
-
-	useMount$(async () => {
+	const searchData = useResource$<SearchData>(async ({ previous }) => {
 		const { search } = activeFacetValueIds.length
 			? await execute<{ search: Search }>(searchQueryWithTerm('', activeFacetValueIds))
 			: await execute<{ search: Search }>(searchQueryWithCollectionSlug(params.slug));
-		state.search = search;
-		if (!state.facedValues.length) {
-			state.facedValues = groupFacetValues(state.search, activeFacetValueIds);
+		let facedValues = previous?.facedValues || [];
+		if (facedValues.length) {
+			facedValues = groupFacetValues(search, activeFacetValueIds);
 		}
-		state.loading = false;
+		return { search, facedValues };
 	});
+	// useMount$(async () => {
+	// 	const { search } = activeFacetValueIds.length
+	// 		? await execute<{ search: Search }>(searchQueryWithTerm('', activeFacetValueIds))
+	// 		: await execute<{ search: Search }>(searchQueryWithCollectionSlug(params.slug));
+	// 	state.search = search;
+	// 	if (!state.facedValues.length) {
+	// 		state.facedValues = groupFacetValues(state.search, activeFacetValueIds);
+	// 	}
+	// 	state.loading = false;
+	// });
 
 	useClientEffect$(async () => {
 		window.scrollTo(0, 0);
@@ -65,9 +84,7 @@ export default component$(() => {
 		state.search = search;
 	});
 
-	return state.loading ? (
-		<>Loading...</>
-	) : (
+	return (
 		<div className="max-w-6xl mx-auto px-4 py-10">
 			<div className="flex justify-between items-center">
 				<h2 className="text-3xl sm:text-5xl font-light tracking-tight text-gray-900 my-8">
@@ -77,13 +94,21 @@ export default component$(() => {
 						onResolved={(collection) => <>{collection ? collection.name : 'All'}</>}
 					/>
 				</h2>
-				{!!state.facedValues.length && (
-					<FiltersButton
-						onToggleMenu$={async () => {
-							state.showMenu = !state.showMenu;
-						}}
-					/>
-				)}
+				<Resource
+					resource={searchData}
+					onPending={() => <p>Loading...</p>}
+					onResolved={(data) => (
+						<>
+							{!!data.facedValues.length && (
+								<FiltersButton
+									onToggleMenu$={async () => {
+										state.showMenu = !state.showMenu;
+									}}
+								/>
+							)}
+						</>
+					)}
+				/>
 			</div>
 
 			<Resource
@@ -107,28 +132,44 @@ export default component$(() => {
 				)}
 			/>
 			<div className="mt-6 grid sm:grid-cols-5 gap-x-4">
-				{!!state.facedValues.length && (
-					<Filters
-						showMenu={state.showMenu}
-						facetsWithValues={state.facedValues}
-						onToggleMenu$={async () => {
-							state.showMenu = !state.showMenu;
-						}}
-						onFilterChange$={onFilterChange}
-					/>
-				)}
+				<Resource
+					resource={searchData}
+					onPending={() => <p>Loading...</p>}
+					onResolved={(data) =>
+						!!data.facedValues.length ? (
+							<Filters
+								showMenu={state.showMenu}
+								facetsWithValues={data.facedValues}
+								onToggleMenu$={async () => {
+									state.showMenu = !state.showMenu;
+								}}
+								onFilterChange$={onFilterChange}
+							/>
+						) : (
+							<></>
+						)
+					}
+				/>
 				<div className="sm:col-span-5 lg:col-span-4">
 					<div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-						{state.search.items?.map((item) => (
-							<ProductCard
-								key={item.productId}
-								productAsset={item.productAsset}
-								productName={item.productName}
-								slug={item.slug}
-								priceWithTax={item.priceWithTax}
-								currencyCode={item.currencyCode}
-							></ProductCard>
-						))}
+						<Resource
+							resource={searchData}
+							onPending={() => <p>Loading...</p>}
+							onResolved={(data) => (
+								<>
+									{data.search.items?.map((item) => (
+										<ProductCard
+											key={item.productId}
+											productAsset={item.productAsset}
+											productName={item.productName}
+											slug={item.slug}
+											priceWithTax={item.priceWithTax}
+											currencyCode={item.currencyCode}
+										></ProductCard>
+									))}
+								</>
+							)}
+						/>
 					</div>
 				</div>
 			</div>
