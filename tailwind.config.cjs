ed5d26838393b340117ed8227d3944c9247bb253
Michael Bromley <michael@michaelbromley.co.uk>
2022-08-23 14:03:37 +0200

    Remove hard-coded styles & update primary color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: colors.blue,
+			},
+		},
 	},
 	plugins: [],
 };
