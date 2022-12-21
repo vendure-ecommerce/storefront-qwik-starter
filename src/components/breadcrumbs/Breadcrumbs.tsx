010c82fff27cf4d3ddb2f947a6d7f3b76c1ca1d4
gioboa <giorgiob.boa@gmail.com>
2022-12-21 16:13:45 +0100

    fix: only with <a> the page will be refresh 🤷‍♂️

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -21,12 +21,12 @@ export default component$<{ items: { name: string; slug: string; id: string }[] 
 						<li key={item.name}>
 							<div class="flex items-center">
 								<SlashIcon />
-								<Link
+								<a
 									href={'/collections/' + item.slug}
 									class="ml-2 md:ml-4 text-xs md:text-sm font-medium text-gray-500 hover:text-gray-700"
 								>
 									{item.name}
-								</Link>
+								</a>
 							</div>
 						</li>
 					))}
