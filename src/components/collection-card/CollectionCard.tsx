c6cadab9ad532c930f40596ae4d8ec8c7b3579ba
gioboa <giorgiob.boa@gmail.com>
2022-12-21 16:13:45 +0100

    fix: only with <a> the page will be refresh 🤷‍♂️

--- src/components/collection-card/CollectionCard.tsx
+++ src/components/collection-card/CollectionCard.tsx
@@ -1,5 +1,4 @@
 import { component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
 import { Collection } from '../../types';
 
 interface IProps {
@@ -8,7 +7,7 @@ interface IProps {
 
 export default component$(({ collection }: IProps) => {
 	return (
-		<Link href={'/collections/' + collection.slug} key={collection.id}>
+		<a href={'/collections/' + collection.slug} key={collection.id}>
 			<div class="max-w-[300px] relative rounded-lg overflow-hidden hover:opacity-75 xl:w-auto mx-auto">
 				<span class="">
 					<div class="w-full h-full object-center object-cover">
@@ -35,6 +34,6 @@ export default component$(({ collection }: IProps) => {
 					{collection.name}
 				</span>
 			</div>
-		</Link>
+		</a>
 	);
 });
