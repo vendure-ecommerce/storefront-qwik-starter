0eac6d494e545896a5d5cc344a9a52392a329ff9
gioboa <giorgiob.boa@gmail.com>
2023-01-20 16:58:35 +0100

    add env variables

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,8 @@ import z from 'zod';
 
 const envVariables = z.object({
 	VITE_VENDURE_PUBLIC_URL: z.string(),
+	VITE_SHOW_PAYMENT_STEP: z.string(),
+	VITE_SHOW_REVIEWS: z.string(),
 });
 
-console.log('import.meta.env', import.meta.env);
 export const ENV_VARIABLES = envVariables.parse(import.meta.env);
