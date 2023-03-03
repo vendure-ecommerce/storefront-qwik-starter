9a4e601e61cde14c85134afe42055c9898f0471f
gioboa <giorgiob.boa@gmail.com>
2023-03-03 15:52:10 +0100

    add server functions

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,30 +1,40 @@
+import { server$ } from '@builder.io/qwik-city';
 import { isBrowser } from '@builder.io/qwik/build';
-import { AUTH_TOKEN } from '~/constants';
+import { AUTH_TOKEN, HEADER_AUTH_TOKEN_KEY } from '~/constants';
 import { ENV_VARIABLES } from '~/env';
 import { getCookie, setCookie } from '.';
 
-export const execute = async <T>(body: {
-	query: string;
-	variables: Record<string, any>;
-}): Promise<T> => {
+type ResponseProps<T> = { token: string; data: T };
+type ExecuteProps = { query: string; variables: Record<string, any> };
+type Options = { method: string; headers: Record<string, string>; body: string };
+
+export const execute = async <T>(body: ExecuteProps): Promise<T> => {
 	let headers: Record<string, string> = { 'Content-Type': 'application/json' };
 	if (isBrowser) {
 		const token = getCookie(AUTH_TOKEN);
 		headers = { ...headers, Authorization: `Bearer ${token}` };
 	}
-	const options = {
-		method: 'POST',
-		headers,
-		body: JSON.stringify(body),
-	};
 
-	const response = await fetch(ENV_VARIABLES.VITE_VENDURE_PUBLIC_URL, options);
-	if (isBrowser) {
-		const responsetoken = response.headers.get('vendure-auth-token');
-		if (responsetoken) {
-			setCookie(AUTH_TOKEN, responsetoken, 365);
-		}
+	const options = { method: 'POST', headers, body: JSON.stringify(body) };
+
+	const response: ResponseProps<T> = isBrowser
+		? await server$(() => executeRequest(options))()
+		: await executeRequest(options);
+
+	if (isBrowser && response.token) {
+		setCookie(AUTH_TOKEN, response.token, 365);
 	}
-	const json: any = await response.json();
-	return json.data;
+
+	return response.data;
+};
+
+const executeRequest = async (options: Options) => {
+	const httpResponse = await fetch(ENV_VARIABLES.VITE_VENDURE_PUBLIC_URL, options);
+	return await extractTokenAndData(httpResponse);
+};
+
+const extractTokenAndData = async (response: Response) => {
+	const token = response.headers.get(HEADER_AUTH_TOKEN_KEY) || '';
+	const { data } = await response.json();
+	return { token, data };
 };
