39f89c63bb3acf231c4a72a4f54f9370bca50550
gioboa <giorgiob.boa@gmail.com>
2023-03-03 15:12:28 +0100

    define constant

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,4 @@ import { AppState } from './types';
 export const APP_STATE = createContextId<AppState>('app_state');
 export const AUTH_TOKEN = 'authToken';
 export const CUSTOMER_NOT_DEFINED_ID = 'CUSTOMER_NOT_DEFINED_ID';
+export const HEADER_AUTH_TOKEN_KEY = 'vendure-auth-token';
