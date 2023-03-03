3fe5fcfe3d49dd198718e12ec282246f9c5aae57
gioboa <giorgiob.boa@gmail.com>
2023-03-03 22:12:01 +0100

    replace Resource with routeLoader

--- src/routes/collections/[...slug]/index.tsx
+++ src/routes/collections/[...slug]/index.tsx
@@ -2,12 +2,10 @@ import {
 	$,
 	component$,
 	QwikKeyboardEvent,
-	Resource,
 	useBrowserVisibleTask$,
-	useResource$,
 	useStore,
 } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { routeLoader$, useLocation } from '@builder.io/qwik-city';
 import Breadcrumbs from '~/components/breadcrumbs/Breadcrumbs';
 import CollectionCard from '~/components/collection-card/CollectionCard';
 import Filters from '~/components/facet-filter-controls/Filters';
@@ -28,10 +26,28 @@ import {
 } from '~/utils';
 import { execute } from '~/utils/api';
 
-export default component$(() => {
-	const { params: _params, url } = useLocation();
-	const params = cleanUpParams(_params);
+export const useCollectionLoader = routeLoader$(async ({ params }) => {
+	const { collection } = await execute<{ collection: Collection }>(getCollectionQuery(params.slug));
+	return collection;
+});
+
+export const searchLoader = routeLoader$(async ({ params: p, url }) => {
+	const params = cleanUpParams(p);
 	const activeFacetValueIds: string[] = url.searchParams.get('f')?.split('-') || [];
+	const { search } = activeFacetValueIds.length
+		? await execute<{ search: Search }>(searchQueryWithTerm(params.slug, '', activeFacetValueIds))
+		: await execute<{ search: Search }>(searchQueryWithCollectionSlug(params.slug));
+	return search;
+});
+
+export default component$(() => {
+	const { params: p, url } = useLocation();
+	const params = cleanUpParams(p);
+	const activeFacetValueIds: string[] = url.searchParams.get('f')?.split('-') || [];
+
+	const collectionSignal = useCollectionLoader.use();
+	const searchSignal = searchLoader.use();
+
 	const state = useStore<{
 		showMenu: boolean;
 		search: Search;
@@ -39,12 +55,12 @@ export default component$(() => {
 		facetValueIds: string[];
 	}>({
 		showMenu: false,
-		search: {} as Search,
-		facedValues: [],
-		facetValueIds: [],
+		search: searchSignal.value,
+		facedValues: groupFacetValues(searchSignal.value, activeFacetValueIds),
+		facetValueIds: activeFacetValueIds,
 	});
 
-	useBrowserVisibleTask$(() => {
+	useBrowserVisibleTask$(async () => {
 		scrollToTop();
 	});
 
@@ -65,22 +81,6 @@ export default component$(() => {
 		state.search = search;
 	});
 
-	const collectionResource = useResource$<Collection>(async () => {
-		const { collection } = await execute<{ collection: Collection }>(
-			getCollectionQuery(params.slug)
-		);
-		return collection;
-	});
-
-	const searchResource = useResource$<void>(async () => {
-		const { search } = activeFacetValueIds.length
-			? await execute<{ search: Search }>(searchQueryWithTerm(params.slug, '', activeFacetValueIds))
-			: await execute<{ search: Search }>(searchQueryWithCollectionSlug(params.slug));
-		state.search = search;
-		state.facedValues = groupFacetValues(search, activeFacetValueIds);
-		state.facetValueIds = activeFacetValueIds;
-	});
-
 	return (
 		<div
 			class="max-w-6xl mx-auto px-4 py-10"
@@ -91,88 +91,58 @@ export default component$(() => {
 			}}
 		>
 			<div class="flex justify-between items-center">
-				<Resource
-					value={collectionResource}
-					onPending={() => <div></div>}
-					onRejected={() => <div></div>}
-					onResolved={(collection) => (
-						<h2 class="text-3xl sm:text-5xl font-light tracking-tight text-gray-900 my-8">
-							{collection?.name}
-						</h2>
+				<h2 class="text-3xl sm:text-5xl font-light tracking-tight text-gray-900 my-8">
+					{collectionSignal.value.name}
+				</h2>
+				<div>
+					{!!state.facedValues.length && (
+						<FiltersButton
+							onToggleMenu$={async () => {
+								state.showMenu = !state.showMenu;
+							}}
+						/>
 					)}
-				/>
-				<Resource
-					value={searchResource}
-					onPending={() => <div></div>}
-					onRejected={() => <div></div>}
-					onResolved={() => (
-						<div>
-							{!!state.facedValues.length && (
-								<FiltersButton
-									onToggleMenu$={async () => {
-										state.showMenu = !state.showMenu;
-									}}
-								/>
-							)}
-						</div>
-					)}
-				/>
+				</div>
 			</div>
-			<Resource
-				value={collectionResource}
-				onPending={() => <div></div>}
-				onRejected={() => <div></div>}
-				onResolved={(collection) => (
-					<div>
-						<Breadcrumbs items={collection?.breadcrumbs || []}></Breadcrumbs>
-						{collection?.children?.length ? (
-							<div class="max-w-2xl mx-auto py-16 sm:py-16 lg:max-w-none border-b mb-16">
-								<h2 class="text-2xl font-light text-gray-900">Collections</h2>
-								<div class="mt-6 grid max-w-xs sm:max-w-none mx-auto sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4">
-									{collection?.children.map((child) => (
-										<CollectionCard key={child.id} collection={child}></CollectionCard>
-									))}
-								</div>
-							</div>
-						) : (
-							''
-						)}
-					</div>
-				)}
-			/>
-			<Resource
-				value={searchResource}
-				onPending={() => <div></div>}
-				onRejected={() => <div></div>}
-				onResolved={() => (
-					<div class="mt-6 grid sm:grid-cols-5 gap-x-4">
-						{!!state.facedValues.length && (
-							<Filters
-								showMenu={state.showMenu}
-								facetsWithValues={state.facedValues}
-								onToggleMenu$={async () => {
-									state.showMenu = !state.showMenu;
-								}}
-								onFilterChange$={onFilterChange}
-							/>
-						)}
-						<div class="sm:col-span-5 lg:col-span-4">
-							<div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-								{state.search.items.map((item) => (
-									<ProductCard
-										key={item.productId}
-										productAsset={item.productAsset}
-										productName={item.productName}
-										slug={item.slug}
-										priceWithTax={item.priceWithTax}
-										currencyCode={item.currencyCode}
-									></ProductCard>
-								))}
-							</div>
+			<div>
+				<Breadcrumbs items={collectionSignal.value.breadcrumbs || []}></Breadcrumbs>
+				{!!collectionSignal.value.children?.length && (
+					<div class="max-w-2xl mx-auto py-16 sm:py-16 lg:max-w-none border-b mb-16">
+						<h2 class="text-2xl font-light text-gray-900">Collections</h2>
+						<div class="mt-6 grid max-w-xs sm:max-w-none mx-auto sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4">
+							{collectionSignal.value.children.map((child) => (
+								<CollectionCard key={child.id} collection={child}></CollectionCard>
+							))}
 						</div>
 					</div>
 				)}
-			/>
+			</div>
+			<div class="mt-6 grid sm:grid-cols-5 gap-x-4">
+				{!!state.facedValues.length && (
+					<Filters
+						showMenu={state.showMenu}
+						facetsWithValues={state.facedValues}
+						onToggleMenu$={async () => {
+							state.showMenu = !state.showMenu;
+						}}
+						onFilterChange$={onFilterChange}
+					/>
+				)}
+				<div class="sm:col-span-5 lg:col-span-4">
+					<div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+						{state.search.items.map((item) => (
+							<ProductCard
+								key={item.productId}
+								productAsset={item.productAsset}
+								productName={item.productName}
+								slug={item.slug}
+								priceWithTax={item.priceWithTax}
+								currencyCode={item.currencyCode}
+							></ProductCard>
+						))}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 });
--- src/routes/products/[...slug]/index.tsx
+++ src/routes/products/[...slug]/index.tsx
@@ -1,14 +1,12 @@
 import {
 	$,
 	component$,
-	Resource,
 	useBrowserVisibleTask$,
 	useContext,
-	useResource$,
 	useStore,
 	useTask$,
 } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
 import Alert from '~/components/alert/Alert';
 import Breadcrumbs from '~/components/breadcrumbs/Breadcrumbs';
 import CheckIcon from '~/components/icons/CheckIcon';
@@ -23,20 +21,27 @@ import { ActiveOrder, Line, Product, Variant } from '~/types';
 import { cleanUpParams, isEnvVariableEnabled, scrollToTop } from '~/utils';
 import { execute } from '~/utils/api';
 
+export const productLoader = routeLoader$(async ({ params }) => {
+	const { slug } = cleanUpParams(params);
+	const { product } = await execute<{ product: Product }>(getProductQuery({ slug }));
+	return product;
+});
+
 export default component$(() => {
-	const location = useLocation();
+	const productSignal = productLoader.use();
+
+	const appState = useContext(APP_STATE);
 	const state = useStore<{
 		product: Product;
 		selectedVariantId: string;
 		quantity: Record<string, number>;
 		addItemToOrderError: string;
 	}>({
-		product: {} as Product,
-		selectedVariantId: '',
+		product: productSignal.value,
+		selectedVariantId: productSignal.value.variants[0].id,
 		quantity: {},
 		addItemToOrderError: '',
 	});
-	const appState = useContext(APP_STATE);
 
 	const calculateQuantities = $((product: Product) => {
 		state.quantity = {};
@@ -49,13 +54,7 @@ export default component$(() => {
 		});
 	});
 
-	const productResource = useResource$<void>(async () => {
-		const { slug } = cleanUpParams(location.params);
-		const { product } = await execute<{ product: Product }>(getProductQuery({ slug }));
-		state.product = product;
-		state.selectedVariantId = product.variants[0].id;
-		calculateQuantities(state.product);
-	});
+	calculateQuantities(state.product);
 
 	useBrowserVisibleTask$(() => {
 		scrollToTop();
@@ -72,152 +71,145 @@ export default component$(() => {
 	return (
 		<div>
 			<div class="max-w-6xl mx-auto px-4 py-10">
-				<Resource
-					value={productResource}
-					onPending={() => <div></div>}
-					onRejected={() => <div></div>}
-					onResolved={() => (
-						<div>
-							<h2 class="text-3xl sm:text-5xl font-light tracking-tight text-gray-900 my-8">
-								{state.product.name}
-							</h2>
-							<Breadcrumbs
-								items={
-									state.product.collections[state.product.collections.length - 1]?.breadcrumbs ?? []
-								}
-							></Breadcrumbs>
-							<div class="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start mt-4 md:mt-12">
-								<div class="w-full max-w-2xl mx-auto sm:block lg:max-w-none">
-									<span class="rounded-md overflow-hidden">
-										<div class="h-[400px] w-full md:w-[400px]">
-											<picture>
-												<source
-													srcSet={state.product.featuredAsset.preview + '?w=400&h=400&format=avif'}
-													type="image/avif"
-												/>
-												<source
-													srcSet={state.product.featuredAsset.preview + '?w=400&h=400&format=webp'}
-													type="image/webp"
-												/>
-												<img
-													src={state.product.featuredAsset.preview + '?w=400&h=400'}
-													alt={state.product.name}
-													class="object-center object-cover rounded-lg"
-													width="400"
-													height="400"
-												/>
-											</picture>
-										</div>
-									</span>
-								</div>
-								<div class="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
-									<div class="">
-										<h3 class="sr-only">Description</h3>
-										<div
-											class="text-base text-gray-700"
-											dangerouslySetInnerHTML={state.product.description}
+				<div>
+					<h2 class="text-3xl sm:text-5xl font-light tracking-tight text-gray-900 my-8">
+						{state.product.name}
+					</h2>
+					<Breadcrumbs
+						items={
+							state.product.collections[state.product.collections.length - 1]?.breadcrumbs ?? []
+						}
+					></Breadcrumbs>
+					<div class="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start mt-4 md:mt-12">
+						<div class="w-full max-w-2xl mx-auto sm:block lg:max-w-none">
+							<span class="rounded-md overflow-hidden">
+								<div class="h-[400px] w-full md:w-[400px]">
+									<picture>
+										<source
+											srcSet={state.product.featuredAsset.preview + '?w=400&h=400&format=avif'}
+											type="image/avif"
 										/>
-									</div>
-									{1 < state.product.variants.length && (
-										<div class="mt-4">
-											<label class="block text-sm font-medium text-gray-700">Select option</label>
-											<select
-												class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm rounded-md"
-												value={state.selectedVariantId}
-												onChange$={(e: any) => (state.selectedVariantId = e.target.value)}
+										<source
+											srcSet={state.product.featuredAsset.preview + '?w=400&h=400&format=webp'}
+											type="image/webp"
+										/>
+										<img
+											src={state.product.featuredAsset.preview + '?w=400&h=400'}
+											alt={state.product.name}
+											class="object-center object-cover rounded-lg"
+											width="400"
+											height="400"
+										/>
+									</picture>
+								</div>
+							</span>
+						</div>
+						<div class="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
+							<div class="">
+								<h3 class="sr-only">Description</h3>
+								<div
+									class="text-base text-gray-700"
+									dangerouslySetInnerHTML={state.product.description}
+								/>
+							</div>
+							{1 < state.product.variants.length && (
+								<div class="mt-4">
+									<label class="block text-sm font-medium text-gray-700">Select option</label>
+									<select
+										class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm rounded-md"
+										value={state.selectedVariantId}
+										onChange$={(e: any) => (state.selectedVariantId = e.target.value)}
+									>
+										{state.product.variants.map((variant) => (
+											<option
+												key={variant.id}
+												value={variant.id}
+												selected={state.selectedVariantId === variant.id}
 											>
-												{state.product.variants.map((variant) => (
-													<option
-														key={variant.id}
-														value={variant.id}
-														selected={state.selectedVariantId === variant.id}
-													>
-														{variant.name}
-													</option>
-												))}
-											</select>
-										</div>
-									)}
-									<div class="mt-10 flex flex-col sm:flex-row sm:items-center">
-										<Price
-											priceWithTax={selectedVariant()?.priceWithTax}
-											currencyCode={selectedVariant()?.currencyCode}
-											forcedClass="text-3xl text-gray-900 mr-4"
-										></Price>
-										<div class="flex sm:flex-col1 align-baseline">
-											<button
-												class={`max-w-xs flex-1 ${
-													state.quantity[state.selectedVariantId] > 7
-														? 'bg-gray-600 cursor-not-allowed'
-														: state.quantity[state.selectedVariantId] === 0
-														? 'bg-primary-600 hover:bg-primary-700'
-														: 'bg-green-600 active:bg-green-700 hover:bg-green-700'
-												} transition-colors border border-transparent rounded-md py-3 px-8 flex items-center 
+												{variant.name}
+											</option>
+										))}
+									</select>
+								</div>
+							)}
+							<div class="mt-10 flex flex-col sm:flex-row sm:items-center">
+								<Price
+									priceWithTax={selectedVariant()?.priceWithTax}
+									currencyCode={selectedVariant()?.currencyCode}
+									forcedClass="text-3xl text-gray-900 mr-4"
+								></Price>
+								<div class="flex sm:flex-col1 align-baseline">
+									<button
+										class={`max-w-xs flex-1 ${
+											state.quantity[state.selectedVariantId] > 7
+												? 'bg-gray-600 cursor-not-allowed'
+												: state.quantity[state.selectedVariantId] === 0
+												? 'bg-primary-600 hover:bg-primary-700'
+												: 'bg-green-600 active:bg-green-700 hover:bg-green-700'
+										} transition-colors border border-transparent rounded-md py-3 px-8 flex items-center 
 									justify-center text-base font-medium text-white focus:outline-none 
 									focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-primary-500 sm:w-full`}
-												onClick$={async () => {
-													if (state.quantity[state.selectedVariantId] <= 7) {
-														const { addItemToOrder: order } = await execute<{
-															addItemToOrder: ActiveOrder;
-														}>(addItemToOrderMutation(state.selectedVariantId, 1));
-														if (order.errorCode) {
-															state.addItemToOrderError = order.errorCode;
-														} else {
-															appState.activeOrder = order;
-														}
-													}
-												}}
-											>
-												{state.quantity[state.selectedVariantId] ? (
-													<span class="flex items-center">
-														<CheckIcon />
-														{state.quantity[state.selectedVariantId]} in cart
-													</span>
-												) : (
-													`Add to cart`
-												)}
-											</button>
+										onClick$={async () => {
+											if (state.quantity[state.selectedVariantId] <= 7) {
+												const { addItemToOrder: order } = await execute<{
+													addItemToOrder: ActiveOrder;
+												}>(addItemToOrderMutation(state.selectedVariantId, 1));
+												if (order.errorCode) {
+													state.addItemToOrderError = order.errorCode;
+												} else {
+													appState.activeOrder = order;
+												}
+											}
+										}}
+									>
+										{state.quantity[state.selectedVariantId] ? (
+											<span class="flex items-center">
+												<CheckIcon />
+												{state.quantity[state.selectedVariantId]} in cart
+											</span>
+										) : (
+											`Add to cart`
+										)}
+									</button>
 
-											<button
-												type="button"
-												class="ml-4 py-3 px-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500"
-											>
-												<HeartIcon />
-												<span class="sr-only">Add to favorites</span>
-											</button>
-										</div>
-									</div>
-									<div class="mt-2 flex items-center space-x-2">
-										<span class="text-gray-500">{selectedVariant()?.sku}</span>
-										<StockLevelLabel stockLevel={selectedVariant()?.stockLevel} />
-									</div>
-									{!!state.addItemToOrderError && (
-										<div class="mt-4">
-											<Alert message={state.addItemToOrderError} />
-										</div>
-									)}
-
-									<section class="mt-12 pt-12 border-t text-xs">
-										<h3 class="text-gray-600 font-bold mb-2">Shipping & Returns</h3>
-										<div class="text-gray-500 space-y-1">
-											<p>
-												Standard shipping: 3 - 5 working days. Express shipping: 1 - 3 working days.
-											</p>
-											<p>
-												Shipping costs depend on delivery address and will be calculated during
-												checkout.
-											</p>
-											<p>
-												Returns are subject to terms. Please see the{' '}
-												<span class="underline">returns page</span> for further information.
-											</p>
-										</div>
-									</section>
+									<button
+										type="button"
+										class="ml-4 py-3 px-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500"
+									>
+										<HeartIcon />
+										<span class="sr-only">Add to favorites</span>
+									</button>
 								</div>
 							</div>
+							<div class="mt-2 flex items-center space-x-2">
+								<span class="text-gray-500">{selectedVariant()?.sku}</span>
+								<StockLevelLabel stockLevel={selectedVariant()?.stockLevel} />
+							</div>
+							{!!state.addItemToOrderError && (
+								<div class="mt-4">
+									<Alert message={state.addItemToOrderError} />
+								</div>
+							)}
+
+							<section class="mt-12 pt-12 border-t text-xs">
+								<h3 class="text-gray-600 font-bold mb-2">Shipping & Returns</h3>
+								<div class="text-gray-500 space-y-1">
+									<p>
+										Standard shipping: 3 - 5 working days. Express shipping: 1 - 3 working days.
+									</p>
+									<p>
+										Shipping costs depend on delivery address and will be calculated during
+										checkout.
+									</p>
+									<p>
+										Returns are subject to terms. Please see the{' '}
+										<span class="underline">returns page</span> for further information.
+									</p>
+								</div>
+							</section>
 						</div>
-					)}
-				/>
+					</div>
+				</div>
 			</div>
 			{isEnvVariableEnabled('VITE_SHOW_REVIEWS') && (
 				<div class="mt-24">
