7325641f6cb66d5805b4b3772e4e15486aa9f685
gioboa <giorgiob.boa@gmail.com>
2023-04-12 23:46:00 +0200

    fix build issue

--- src/components/cart-contents/CartContents.tsx
+++ src/components/cart-contents/CartContents.tsx
@@ -46,92 +46,95 @@ export default component$<{
 	return (
 		<div class="flow-root">
 			<ul class="-my-6 divide-y divide-gray-200">
-				{rowsSignal.value.map(({ linePriceWithTax, ...line }, key) => (
-					<li key={key} class="py-6 flex">
-						<div class="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
-							<Image
-								layout="fixed"
-								width="100"
-								height="100"
-								class="w-full h-full object-center object-cover"
-								src={line.featuredAsset?.preview + '?preset=thumb'}
-								alt={line.productVariant.name}
-								placeholder={IMAGE_PLACEHOLDER_BACKGROUND}
-							/>
-						</div>
+				{rowsSignal.value.map((line, key) => {
+					const { linePriceWithTax } = line;
+					return (
+						<li key={key} class="py-6 flex">
+							<div class="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
+								<Image
+									layout="fixed"
+									width="100"
+									height="100"
+									class="w-full h-full object-center object-cover"
+									src={line.featuredAsset?.preview + '?preset=thumb'}
+									alt={line.productVariant.name}
+									placeholder={IMAGE_PLACEHOLDER_BACKGROUND}
+								/>
+							</div>
 
-						<div class="ml-4 flex-1 flex flex-col">
-							<div>
-								<div class="flex justify-between text-base font-medium text-gray-900">
-									<h3>
-										<Link href={`/products/${line.productVariant.product.slug}/`}>
-											{line.productVariant.name}
-										</Link>
-									</h3>
-									<Price
-										priceWithTax={linePriceWithTax}
-										currencyCode={currencyCode}
-										forcedClass="ml-4"
-									></Price>
-								</div>
-							</div>
-							<div class="flex-1 flex items-center text-sm">
-								{isInEditableUrl ? (
-									<form>
-										<label html-for={`quantity-${line.id}`} class="mr-2">
-											Quantity
-										</label>
-										<select
-											disabled={!isInEditableUrl}
-											id={`quantity-${line.id}`}
-											name={`quantity-${line.id}`}
-											value={line.quantity}
-											onChange$={async (e: QwikChangeEvent<HTMLSelectElement>) => {
-												currentOrderLineSignal.value = { id: line.id, value: +e.target?.value };
-											}}
-											class="max-w-full rounded-md border border-gray-300 py-1.5 text-base leading-5 font-medium text-gray-700 text-left shadow-sm focus:outline-none focus:ring-1 focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
-										>
-											<option value={1}>1</option>
-											<option value={2}>2</option>
-											<option value={3}>3</option>
-											<option value={4}>4</option>
-											<option value={5}>5</option>
-											<option value={6}>6</option>
-											<option value={7}>7</option>
-											<option value={8}>8</option>
-										</select>
-									</form>
-								) : (
-									<div class="text-gray-800">
-										<span class="mr-1">Quantity</span>
-										<span class="font-medium">{line.quantity}</span>
+							<div class="ml-4 flex-1 flex flex-col">
+								<div>
+									<div class="flex justify-between text-base font-medium text-gray-900">
+										<h3>
+											<Link href={`/products/${line.productVariant.product.slug}/`}>
+												{line.productVariant.name}
+											</Link>
+										</h3>
+										<Price
+											priceWithTax={linePriceWithTax}
+											currencyCode={currencyCode}
+											forcedClass="ml-4"
+										></Price>
 									</div>
-								)}
-								<div class="flex-1"></div>
-								<div class="flex">
-									{isInEditableUrl && (
-										<button
-											value={line.id}
-											class="font-medium text-primary-600 hover:text-primary-500"
-											onClick$={async () => {
-												appState.activeOrder = await removeOrderLineMutation(line.id);
-												if (
-													appState.activeOrder?.lines?.length === 0 &&
-													isCheckoutPage(location.url.toString())
-												) {
-													appState.showCart = false;
-													navigate(`/`);
-												}
-											}}
-										>
-											Remove
-										</button>
+								</div>
+								<div class="flex-1 flex items-center text-sm">
+									{isInEditableUrl ? (
+										<form>
+											<label html-for={`quantity-${line.id}`} class="mr-2">
+												Quantity
+											</label>
+											<select
+												disabled={!isInEditableUrl}
+												id={`quantity-${line.id}`}
+												name={`quantity-${line.id}`}
+												value={line.quantity}
+												onChange$={async (e: QwikChangeEvent<HTMLSelectElement>) => {
+													currentOrderLineSignal.value = { id: line.id, value: +e.target?.value };
+												}}
+												class="max-w-full rounded-md border border-gray-300 py-1.5 text-base leading-5 font-medium text-gray-700 text-left shadow-sm focus:outline-none focus:ring-1 focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+											>
+												<option value={1}>1</option>
+												<option value={2}>2</option>
+												<option value={3}>3</option>
+												<option value={4}>4</option>
+												<option value={5}>5</option>
+												<option value={6}>6</option>
+												<option value={7}>7</option>
+												<option value={8}>8</option>
+											</select>
+										</form>
+									) : (
+										<div class="text-gray-800">
+											<span class="mr-1">Quantity</span>
+											<span class="font-medium">{line.quantity}</span>
+										</div>
 									)}
+									<div class="flex-1"></div>
+									<div class="flex">
+										{isInEditableUrl && (
+											<button
+												value={line.id}
+												class="font-medium text-primary-600 hover:text-primary-500"
+												onClick$={async () => {
+													appState.activeOrder = await removeOrderLineMutation(line.id);
+													if (
+														appState.activeOrder?.lines?.length === 0 &&
+														isCheckoutPage(location.url.toString())
+													) {
+														appState.showCart = false;
+														navigate(`/`);
+													}
+												}}
+											>
+												Remove
+											</button>
+										)}
+									</div>
 								</div>
 							</div>
-						</div>
-					</li>
-				))}
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
