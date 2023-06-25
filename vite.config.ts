f03f6218a6e5401f712aa616e382c28d49247538
gioboa <giorgiob.boa@gmail.com>
2023-06-25 18:46:09 +0200

    add insightsEntryStrategy build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
+import { insightsEntryStrategy } from '@builder.io/qwik-labs/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => {
+export default defineConfig(async () => {
 	return {
 		ssr: { target: 'webworker' },
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		plugins: [
+			qwikCity(),
+			qwikVite({
+				entryStrategy: await insightsEntryStrategy({
+					publicApiKey: loadEnv('', '.').VITE_QWIK_INSIGHTS_KEY,
+				}),
+			}),
+			tsconfigPaths(),
+		],
 		preview: {
 			headers: {
 				'Cache-Control': 'public, max-age=600',
