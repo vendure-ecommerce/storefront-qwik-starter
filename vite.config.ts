81c12156e04cde846ffa76d3bca7128eed2de049
gioboa <giorgiob.boa@gmail.com>
2023-08-14 15:36:01 +0200

    add insightsEntryStrategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
+import { insightsEntryStrategy } from '@builder.io/qwik-labs/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig((config) => {
+export default defineConfig(async (config) => {
 	return {
 		// Enable to analyze via source-map-explorer
 		ssr: { target: 'webworker' },
 		build: {
 			sourcemap: config.mode === 'development',
 		},
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		plugins: [
+			qwikCity(),
+			qwikVite({
+				entryStrategy: await insightsEntryStrategy({
+					publicApiKey: loadEnv('', '.').VITE_QWIK_INSIGHTS_KEY,
+				}),
+			}),
+			tsconfigPaths(),
+		],
 		preview: {
 			headers: {
 				'Cache-Control': 'public, max-age=600',
