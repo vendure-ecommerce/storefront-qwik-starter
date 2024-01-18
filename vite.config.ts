730d1a372a674c8c274d9fd225df3b32e3d48450
gioboa <giorgiob.boa@gmail.com>
2024-01-18 19:50:29 +0100

    refactor: remove Insights

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
-import { qwikInsights } from '@builder.io/qwik-labs/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(async (config) => {
+export default defineConfig((config) => {
 	return {
 		// Enable to analyze via source-map-explorer
 		ssr: { target: 'webworker' },
 		build: {
 			sourcemap: config.mode === 'development',
 		},
-		plugins: [
-			qwikInsights({
-				publicApiKey: loadEnv('', '.', '').VITE_QWIK_INSIGHTS_KEY,
-			}),
-			qwikCity(),
-			qwikVite(),
-			tsconfigPaths(),
-		],
+		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
 		preview: {
 			headers: {
 				'Cache-Control': 'public, max-age=600',
