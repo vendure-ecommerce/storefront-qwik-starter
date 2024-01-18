ff6c8da30677b9687d26d5a5b8b47606dda7ffd6
gioboa <giorgiob.boa@gmail.com>
2024-01-18 19:56:05 +0100

    Revert "refactor: remove Insights"
    This reverts commit 730d1a372a674c8c274d9fd225df3b32e3d48450.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
+import { qwikInsights } from '@builder.io/qwik-labs/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig((config) => {
+export default defineConfig(async (config) => {
 	return {
 		// Enable to analyze via source-map-explorer
 		ssr: { target: 'webworker' },
 		build: {
 			sourcemap: config.mode === 'development',
 		},
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		plugins: [
+			qwikInsights({
+				publicApiKey: loadEnv('', '.', '').VITE_QWIK_INSIGHTS_KEY,
+			}),
+			qwikCity(),
+			qwikVite(),
+			tsconfigPaths(),
+		],
 		preview: {
 			headers: {
 				'Cache-Control': 'public, max-age=600',
