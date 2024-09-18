64ed4946d456efc68c539cdc2d745363f6fa4d88
gioboa <giorgiob.boa@gmail.com>
2024-09-18 13:07:56 +0200

    chore: remove VITE_QWIK_INSIGHTS_KEY env

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,6 @@ const envVariables = z.object({
 	VITE_SHOW_REVIEWS: z.string(),
 	VITE_SECURE_COOKIE: z.string(),
 	VITE_STRIPE_PUBLISHABLE_KEY: z.string(),
-	VITE_QWIK_INSIGHTS_KEY: z.string(),
 });
 
 export const ENV_VARIABLES = envVariables.parse(import.meta.env);
