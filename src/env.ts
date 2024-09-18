db120ab4ca0a8708b139b4da2fc5ce6ae343c46e
Friedger <mail@friedger.de>
2024-09-18 13:07:02 +0200

    fix: add read_only_instance flag (#164)

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ const envVariables = z.object({
 	VITE_VENDURE_PROD_URL: z.string(),
 	VITE_VENDURE_LOCAL_URL: z.string(),
 	VITE_VENDURE_DEV_URL: z.string(),
+	VITE_IS_READONLY_INSTANCE: z.string(),
 	VITE_SHOW_PAYMENT_STEP: z.string(),
 	VITE_SHOW_REVIEWS: z.string(),
 	VITE_SECURE_COOKIE: z.string(),
